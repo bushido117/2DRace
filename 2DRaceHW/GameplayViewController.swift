4b611f5e46b5b84f6ce0bb77f005e950227c2925
bushido117 <vadim.saiko53370@mail.ru>
2022-09-21 10:05:36 +0300

    add project

---
+++ 2DRaceHW/GameplayViewController.swift
@@ -0,0 +1,111 @@
+//
+//  GameplayViewController.swift
+//  2DRaceHW
+//
+//  Created by Вадим Сайко on 19.09.22.
+//
+
+import UIKit
+
+protocol GameplayViewControllerDelegate: AnyObject {
+    func scoreCounterToVC(_ scoreCounter: Int)
+}
+
+class GameplayViewController: UIViewController {
+    weak var gameplayVCDelegate: GameplayViewControllerDelegate?
+    lazy var myCar: UIImageView = {
+        let myCar = UIImageView(image: UIImage(named: "myCar"))
+        myCar.frame = CGRect(x: 15, y: 730, width: 50, height: 75)
+        return myCar
+    }()
+    lazy var moveMyCarGesture: UIPanGestureRecognizer = {
+        let gesture = UIPanGestureRecognizer()
+        gesture.addTarget(self, action: #selector(gestureTapped))
+        return gesture
+    }()
+    lazy var road: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.frame = UIScreen.main.bounds
+        return view
+    }()
+    var roadStripTimer: Timer?
+    var carsTimer: Timer?
+    var scoreCounter = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(road)
+        view.addSubview(myCar)
+        view.addGestureRecognizer(moveMyCarGesture)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        roadStripTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(createRoadStrip), userInfo: nil, repeats: true)
+        carsTimer = Timer.scheduledTimer(timeInterval: 0.6, target: self, selector: #selector(createCars), userInfo: nil, repeats: true)
+    }
+    
+    @objc func createRoadStrip() {
+        for i in 1...3 {
+            let view = UIView(frame: CGRect(x: 97.5 * Double(i), y: -100, width: 10, height: 40))
+                view.backgroundColor = .white
+            UIView.animate(withDuration: 2) { [unowned self] in
+                view.frame.origin.y += 1100
+                self.road.addSubview(view)
+            } completion: { _ in
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
+    @objc func gestureTapped() {
+        let translation = moveMyCarGesture.translation(in: self.view)
+        let newX = myCar.center.x + translation.x
+        myCar.center.x = newX
+        moveMyCarGesture.setTranslation(CGPoint.zero, in: self.view)
+    }
+
+    @objc func createCars() {
+        let policeCar = UIImageView(image: UIImage(named: "policeCar"))
+        policeCar.frame.size = CGSize(width: 50, height: 75)
+        let trafficLine = Int.random(in: 0...3)
+        if trafficLine == 0 {
+            policeCar.frame.origin = CGPoint(x: 15, y: -200)
+        } else if trafficLine == 1 {
+            policeCar.frame.origin = CGPoint(x: 125, y: -200)
+        } else if trafficLine == 2 {
+            policeCar.frame.origin = CGPoint(x: 222, y: -200)
+        } else if trafficLine == 3 {
+            policeCar.frame.origin = CGPoint(x: 320, y: -200)
+        }
+        self.view.addSubview(policeCar)
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] intersectsTimer in
+            policeCar.frame.origin.y += 7
+            if policeCar.frame.intersects(self?.myCar.frame ?? CGRect(x: 0, y: 0, width: 0, height: 0)) {
+                self?.moveMyCarGesture.isEnabled = false
+                let backToMenuButton = UIButton()
+                backToMenuButton.setTitle("Back to menu", for: .normal)
+                backToMenuButton.backgroundColor = .red
+                backToMenuButton.frame.size = CGSize(width: 200, height: 100)
+                backToMenuButton.addTarget(self, action: #selector(self?.backToMenuButtonTap), for: .touchUpInside)
+                self?.view.addSubview(backToMenuButton)
+                backToMenuButton.center = self?.view.center ?? CGPoint(x: 0, y: 0)
+                intersectsTimer.invalidate()
+                self?.roadStripTimer?.invalidate()
+                self?.carsTimer?.invalidate()
+                self?.roadStripTimer = nil
+                self?.carsTimer = nil
+            }
+            if policeCar.frame.origin.y > 1100 {
+                policeCar.removeFromSuperview()
+                self?.scoreCounter += 1
+                intersectsTimer.invalidate()
+            }
+        }
+    }
+    @objc func backToMenuButtonTap() {
+        gameplayVCDelegate?.scoreCounterToVC(scoreCounter)
+        dismiss(animated: true)
+    }
+}
--- 2DRaceHW/ViewController.swift
+++ 2DRaceHW/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GameplayViewControllerDelegate {
+    var highScore = 0
+    lazy var playButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 130, y: 300, width: 150, height: 50)
+        button.backgroundColor = .yellow
+        button.setTitle("Play", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 30)
+        button.layer.cornerRadius = 20
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 4
+        button.addTarget(self, action: #selector(playRaceGame(_:)), for: .touchUpInside)
+        return button
+    }()
+    lazy var recordsLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 130, y: 400, width: 150, height: 50)
+        label.backgroundColor = .yellow
+        label.text = "  High score:"
+        label.textColor = .black
+        label.layer.cornerRadius = 20
+        label.layer.borderColor = UIColor.black.cgColor
+        label.layer.borderWidth = 4
+        label.clipsToBounds = true
+        return label
+    }()
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "road"))
+        imageView.contentMode = .scaleToFill
+        imageView.frame = UIScreen.main.bounds
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(imageView)
+        view.addSubview(playButton)
+        view.addSubview(recordsLabel)
     }
-
-
+    
+    @objc func playRaceGame (_ action: Any) {
+        let vc = GameplayViewController()
+        vc.gameplayVCDelegate = self
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
+    
+    func scoreCounterToVC(_ scoreCounter: Int) {
+        if scoreCounter > highScore {
+            highScore = scoreCounter
+            self.recordsLabel.text = "  High score: \(highScore)"
+        }
+    }
 }
 
